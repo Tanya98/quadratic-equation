fa864396ba144061db4881ec62fd11471fe70b81
Tanya98 <31532666+Tanya98@users.noreply.github.com>
2018-09-25 19:50:40 +0600

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  debugger;
+  let a, b, c;
+  [a, b, c] = getNum(equation);
+  return getList(a, b, c);
+}
+function getNum(equation) {
+  let a = equation.split(' * x^2')[0];
+  let a_sign = equation.split(a + '* x^2')[0];
+  a = parseInt(a_sign + a);
+
+  let b_sign = equation.split('* x^2 ')[1].charAt(0);
+  let b = equation.split(' * x^2 ' + b_sign + ' ')[1].split(' * x ')[0];
+  b = parseInt(b_sign + b);
+
+  let c_sign = equation.split(' * x ')[1].charAt(0);
+  let c = equation.split('* x ' + c_sign + ' ')[1];
+  c = parseInt(c_sign + c);
+  return [a, b, c];
+}
+function getList(a, b, c) {
+  let list = [];
+  list.push(getX('+', a, b, c));
+  list.push(getX('-', a, b, c));
+  list.sort((a, b) => a - b);
+  return list;
+}
+function getX(sign, a, b, c) {
+  let x, b1, a1, c1;
+  b1 = parseInt(- + b);
+  a1 = 2 * a;
+  c1 = Math.sqrt((b * b) - 4 * a * c);
+  return x = Math.round((sign === '+') ? ((b1 + c1) / a1) : ((b1 - c1) / a1));
 }
